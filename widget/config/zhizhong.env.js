08bd8d34c9e3e082278d0da4f0136f0fefb90aca
yanmin <yanmin012022@163.com>
2022-04-22 14:14:00 +0800

    config

--- widget/config/zhizhong.env.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = {
-  NODE_ENV: '"production"',
-  ENV_CONFIG: '"zhizhong"',
-  firmId: '"zhizhong"',
-  finderName: '"zhizhong"',
-  appid: '"wx9390904a6dc6c377"',
-  baseURL: '"https://hospital.zz-med.com/"', //线上环境
-};
--- widget/src/axios/base.js
+++
@@ -1,9 +0,0 @@
-/**
- * 接口域名的管理
- */
-const base = {
-  sq: 'https://llootong.net/his/',
-  bd: 'https://llootong.net/'
-}
-
-export default base;
